91a7cc9bab7136efd606b89469466cd802f730b5
Kadir <kegilmez@gmail.com>
2019-05-11 22:51:18 +0100

    componentWillUnmount added

--- src/App.js
+++ src/App.js
@@ -20,7 +20,18 @@ const imageSrc7 =
   "https://media.istockphoto.com/photos/process-of-creating-drawing-in-ebru-technique-painting-on-the-water-picture-id995247330?s=2048x2048";
 const imageSrc8 =
   "https://media.istockphoto.com/photos/ebru-art-traditional-turkish-ebru-technique-picture-id501911650?s=2048x2048";
-
+const imageSrc9 =
+  "https://live.staticflickr.com/7621/26840222086_7e45bdf193_b.jpg";
+const imageSrc10 =
+  "https://live.staticflickr.com/7484/26780827862_18a28aabbf_h.jpg";
+const imageSrc11 =
+  "https://live.staticflickr.com/7202/26270024163_401631bf08_h.jpg";
+const imageSrc12 =
+  "https://live.staticflickr.com/7295/26840215746_8e85b88e9d_h.jpg";
+let imageSrc13 =
+  "https://live.staticflickr.com/7198/26270039683_8a53ad0fa4_h.jpg";
+const imageSrc14 =
+  "https://live.staticflickr.com/7791/26601190840_3153a8a876_h.jpg";
 const imageSources = [
   imageSrc1,
   imageSrc2,
@@ -29,7 +40,13 @@ const imageSources = [
   imageSrc5,
   imageSrc6,
   imageSrc7,
-  imageSrc8
+  imageSrc8,
+  imageSrc9,
+  imageSrc10,
+  imageSrc11,
+  imageSrc12,
+  imageSrc13,
+  imageSrc14
 ];
 
 export default class App extends Component {
@@ -37,11 +54,16 @@ export default class App extends Component {
     super(props);
     this.state = {
       index: 0,
-      speed: 3000
+      speed: 2000
     };
   }
   componentDidMount() {
     this.getAutoForward();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.autoForward);
+    clearInterval(this.autoBackwards);
   }
 
   getBackwards = () => {
@@ -92,25 +114,17 @@ export default class App extends Component {
         onClick: this.getAutoForward
       }
     ];
+    const newIndex =
+      index >= 0
+        ? index % imageSources.length
+        : imageSources.length - 1 + (index % imageSources.length);
+
     return (
       <div className="xs-col-12 sm-col-10 container">
         <div style={{ position: "relative" }}>
-          <Image
-            imgSrc={
-              imageSources[
-                index >= 0
-                  ? index % imageSources.length
-                  : imageSources.length - 1 + (index % imageSources.length)
-              ]
-            }
-          />
-
+          <Image imgSrc={imageSources[newIndex]} />
           <p style={paraStyle}>
-            {(index >= 0
-              ? index % imageSources.length
-              : imageSources.length - 1 + (index % imageSources.length)) +
-              1}{" "}
-            / {imageSources.length}
+            {newIndex + 1} / {imageSources.length}
           </p>
         </div>
         <div className="center" style={{ marginTop: "15px" }}>
